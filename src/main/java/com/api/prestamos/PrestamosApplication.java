e2623e02ac6a662af5c0a4beec0845d711e66614
Danielvaron1 <dani-0153@hotmail.com>
2024-07-03 21:25:45 -0500

    railway

--- src/main/java/com/api/prestamos/PrestamosApplication.java
+++ src/main/java/com/api/prestamos/PrestamosApplication.java
@@ -1,12 +1,20 @@
 package com.api.prestamos;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class PrestamosApplication {
 
 	public static void main(String[] args) {
+		// Retrieve execution profile from environment variable. If not present, default profile is selected.
+		String profile = System.getenv("PROFILE");
+		System.setProperty("spring.profiles.active", profile != null ? profile : "default");
+		// Railway's internal interface takes some time to start. We wait for it to be ready.
+		log.debug("Waiting fo Internal Interface to start...");
+
 		SpringApplication.run(PrestamosApplication.class, args);
 	}
 
